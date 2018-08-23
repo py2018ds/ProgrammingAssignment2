0f542facb84eb35b20d4470d00e1dda86bba757e
py2018ds <peiyuanxu71@gmail.com>
2018-08-22 20:34:39 -0500

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    #Matrix changed, set inverse to null
+    x <<- y
+    inverse <<- NULL  
+  }
+  #retrieve matrix
+  get <- function() x  
+  #store inverse
+  setinverse <- function(inv) inverse <<- inv 
+  #retrieve inverse
+  getinverse <- function() inverse            
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #retrieve cached inverse
+  inverse <- x$getinverse()  
+  if(!is.null(inverse)) {  
+    #cached inverse is not null, return it
+    message("getting cached data")
+    return(inverse)  
+  }
+  #get the matrix
+  data <- x$get()    
+  #calculate its inverse
+  inverse <- solve(data, ...)  
+  #store the inverse to cache
+  x$setinverse(inverse)      
+  #return calculated inverse
+  inverse		               
 }
+
+m <- matrix(data = 1:4, nrow = 2, ncol = 2, byrow = FALSE,
+            dimnames = NULL)
+
+#test functions
+x <- makeCacheMatrix(m)
+#first time, calculating mean and saving to cache
+cacheSolve(x)
+
+
+#second time, getting cached data
+minv <- cacheSolve(x)
+print(minv)
+
+
+m <- matrix(data = 2:5, nrow = 2, ncol = 2, byrow = FALSE,
+            dimnames = NULL)
+#change the matrix, clear cache
+x$set(m)
+
+#first time, calculating mean and saving to cache
+cacheSolve(x)
+
+#second time, getting cached data
+cacheSolve(x)
+
+
+
+
+
